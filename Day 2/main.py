a00d648c2c0f4aca1069712a975c0a1462cba40d
Samuel Bodkin <Samuel.Bodkin@Cognizant.com>
2023-12-05 14:43:57 +0000

    Optmisation v2

--- Day 2/main.py
+++ Day 2/main.py
@@ -1,15 +1,15 @@
-data = open('Day 2\Data.txt', 'r')
+with open('Day 2\Data.txt', 'r') as data:
+    lines = data.readlines()
 
 # only 12 red cubes, 13 green cubes, and 14 blue cubes
 #Game 1: 6 green, 3 blue; 3 red, 1 green; 4 green, 3 red, 5 blue
 cubeBagDict = {"green" : 13, "blue" : 14, "red" : 12}
 Total = 0
-for x in data:
-    x = x.rstrip()
+for line in lines:
     gameState = True
     cubeDict = {"green" : 0, "blue" : 0, "red" : 0}
 
-    game, dataRow = (x.split(":"))
+    game, dataRow = line.rstrip().split(":")
     for dataSet in dataRow.split(";"):
         for x in dataSet.split(","):
             _ , value,key = x.split(" ")
@@ -20,7 +20,7 @@ for x in data:
                 gameState = False
                 break
 
-    if gameState == True:
+    if gameState:
         Total += int(game.split(" ")[1])
 print(Total)
 
