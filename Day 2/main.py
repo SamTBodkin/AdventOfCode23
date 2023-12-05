a0c298a27d1bec0941cbd2cdbc2a1ff96a43813c
Samuel Bodkin <Samuel.Bodkin@Cognizant.com>
2023-12-05 14:18:30 +0000

    Completed Day 2

---
+++ Day 2/main.py
@@ -0,0 +1,36 @@
+data = open('Day 2\Data.txt', 'r')
+
+# only 12 red cubes, 13 green cubes, and 14 blue cubes
+#Game 1: 6 green, 3 blue; 3 red, 1 green; 4 green, 3 red, 5 blue
+key = []
+value = []
+cubeBagDict = {"green" : 13, "blue" : 14, "red" : 12}
+Total = 0
+for x in data:
+    gameState = True
+    cubeDict = {"green" : 0, "blue" : 0, "red" : 0}
+    x = x.rstrip()
+    # Getting data extracted from the line 
+    game, dataRow = (x.split(":"))
+    print(game)
+    dataRow = dataRow.split(";")
+    print(dataRow)
+
+    for x in dataRow:
+        dataSet = x.split(";")
+        for x in dataSet:
+            cubes = x.split(",")
+            for x in cubes:
+                z , value,key = x.split(" ")
+                cubeDict[key] = int(value)
+
+            if((cubeDict["green"] <= cubeBagDict["green"]) & (cubeDict["red"] <= cubeBagDict["red"]) & (cubeDict["blue"] <= cubeBagDict["blue"])):
+                print("Successful Game")
+                pass
+            else:
+                print("Unsuccessful Game")
+                gameState = False
+    if gameState == True:
+        Total += int(game.split(" ")[1])
+    print(Total)
+
